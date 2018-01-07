4293881493031703c091dae49f372ce5d00eadce
Pavel Andreev <pandreev.zel@gmail.com>
2018-01-07 16:57:38 +0300

    Play with redux react counter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import Counter from './counter/components/Counter';
+import counter from './counter/reducers';
 import App from './App';
 
 ReactDOM.render(
   <App cat={5} />,
   document.querySelector('#root')
 );
+
+const store = createStore(counter);
+
+const render = () => ReactDOM.render(
+  <Counter
+    value={store.getState()}
+    onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+    onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+  />,
+  document.querySelector('#counter')
+);
+
+render();
+store.subscribe(render);
